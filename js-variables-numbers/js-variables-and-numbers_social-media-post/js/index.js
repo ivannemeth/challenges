22172dbf2910598c95bb9c01f3cb4d4ac20228cd
ivannemeth <nemethivn@gmail.com>
2023-11-02 12:05:49 +0100

    finished task

--- js-variables-numbers/js-variables-and-numbers_social-media-post/js/index.js
+++ js-variables-numbers/js-variables-and-numbers_social-media-post/js/index.js
@@ -1,4 +1,4 @@
-console.clear();
+console.log("HERE WE ARE");
 
 /*
 1. Create the data for a small social media post. Create a const variable for each data point listed below.:
@@ -7,9 +7,17 @@ console.clear();
 	- the number of likes
 	- the user who created the post
 	- a boolean called isReported
-*/
+	*/
 
 // --v-- write your code here --v--
+
+const title = "This is the title";
+const text = "blablabla";
+let numberOfLikes = 24;
+const userName = "Ivan Nemeth";
+const isReported = true;
+
+const MediaPost = [title, text, numberOfLikes, userName, isReported];
 
 // --^-- write your code here --^--
 
@@ -18,5 +26,17 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+
+// Option 1: console.log each const
+
+//console.log("title:", title); etc
+
+//Option 2: consol.log in one line separated by commas and ""
+
+//console.log("title:", title, "", "text:", text, "", "likes:", numberOfLikes); etc
+
+//Option 3: console.log as an array
+console.log("mediapost", MediaPost);
+console.log("likes", numberOfLikes + 1);
 
 // --^-- write your code here --^--
