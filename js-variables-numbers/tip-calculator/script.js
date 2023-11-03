38c69409dd4b7b2f6960abb546a2a0a18cf6b089
ivannemeth <nemethivn@gmail.com>
2023-11-03 17:39:04 +0100

    finished the task

---
+++ js-variables-numbers/tip-calculator/script.js
@@ -0,0 +1,8 @@
+const mealCost = 30;
+const tipPercentage = 10;
+
+let tipAmount = (mealCost * tipPercentage) / 100;
+
+let totalCost = mealCost + tipAmount;
+
+console.log("The total const of your meal is:", totalCost, "euros");
