0a2440b6ad0b16e84bc10b13ae8c1f75b8feede9
ivannemeth <nemethivn@gmail.com>
2023-11-06 15:20:34 +0100

    finised challenge

--- js-booleans/js-conditions-and-booleans_booleans/js/index.js
+++ js-booleans/js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,43 @@
 console.clear();
 
 console.log(Boolean("Try it out"));
+/** 
+2 == "2" - true;
+2 === "2";
+-false;
+2 === 2;
+-true("hello world");
+false("");
+false;
+true;
+true;
+0 
+- 50;
+50;
+5 > 1;
+("1 > 10");
+("false");
+*/
+console.log(Boolean(2 == "2"));
+
+console.log(Boolean(2 === "2"));
+
+console.log(Boolean("Hello world"));
+
+console.log(Boolean(""));
+
+console.log(Boolean(false));
+
+console.log(Boolean(true));
+
+console.log(Boolean(0));
+
+console.log(Boolean(-50));
+
+console.log(Boolean(50));
+
+console.log(Boolean(5 > 1));
+
+console.log(Boolean("1 > 10"));
+
+console.log(Boolean("false"));
