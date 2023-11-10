aeded508d38cd5307e97bfe7368c18f51a367c19
ivannemeth <nemethivn@gmail.com>
2023-11-10 14:05:45 +0100

    checkbox

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -28,6 +29,7 @@ form.addEventListener("submit", (event) => {
 
   if (tosCheckbox) {
     alert("Form submitted");
+    success.removeAttribute("hidden");
   }
 });
 
