00250653b18e0da0fcf692fea3eaa425409a4618
ivannemeth <nemethivn@gmail.com>
2023-11-10 12:21:13 +0100

    checkbox

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -12,13 +12,27 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+tosCheckbox.addEventListener("input", (event) => {
+  event.preventDefault();
+  if (event.target.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
   // --v-- write your code here --v--
+  const tosCheckbox = event.target.elements.tos.checked;
 
-  // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  if (tosCheckbox) {
+    alert("Form submitted");
+  }
 });
+
+/*console.log(tosCheckbox.checked);*/
+
+// --^-- write your code here --^--
+
+// eslint-disable-next-line no-alert
