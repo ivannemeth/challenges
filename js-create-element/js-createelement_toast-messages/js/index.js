ea81131a60877028eb3b525d43266b39b179d427
ivannemeth <nemethivn@gmail.com>
2023-11-10 15:58:44 +0100

    toast messages

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,17 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (e) => {
   // Exercise: Append a new entry to the toast messages container
+  e.preventDefault();
+  const li = document.createElement("li");
+  li.textContent = "Toast message 2";
+
+  li.classList.add("toast-container__message");
+  toastContainer.append(li);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
