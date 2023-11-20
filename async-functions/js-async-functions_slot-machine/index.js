285a9c9465a26502c4c2d16ca279e43803683869
ivannemeth <nemethivn@gmail.com>
2023-11-20 12:11:09 +0100

    slot machine - not finished

--- async-functions/js-async-functions_slot-machine/index.js
+++ async-functions/js-async-functions_slot-machine/index.js
@@ -2,6 +2,7 @@ import { Wheel } from "./components/Wheel/Wheel.js";
 import { SpinButton } from "./components/SpinButton/SpinButton.js";
 import { Machine } from "./components/Machine/Machine.js";
 import { Result } from "./components/Result/Result.js";
+import { getMaxCount } from "./utils/symbols.js";
 
 console.clear();
 
@@ -22,6 +23,30 @@ root.append(machine, spinButton, result);
 //                                      ↙️
 spinButton.addEventListener("click", async () => {
   spinButton.disabled = true;
+try {
+  const symbols = await Promise.all([
+    wheel1.spin(),
+    wheel2.spin(),
+    wheel3.spin(),
+  ]);
+
+  const sameSymbols = getMaxCount(symbols);
+
+if sameSymbols === 
+
+
+
+
+
+}
+catch (error) {
+  console.log(error);
+}
+finally {
+  console.log("done");
+}
+  
+  //console.log(values);
   /**
    * Hint 1:
    * The wheel elements have a spin method that returns a promise.
