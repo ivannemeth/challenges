5af13bd078623536e5054209eb8248e7d256d21f
ivannemeth <nemethivn@gmail.com>
2023-11-21 11:58:04 +0100

    star wars console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,22 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const r2d2 = data.results[2];
+      //console.log(data);
+      //console.log(data.results[6]);
+      console.log(r2d2.eye_color);
+    } else {
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    console.error("An Error occurred");
+  }
+}
 
 fetchData();
