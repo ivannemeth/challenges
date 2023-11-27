e61d5221623fbfb26c98079fb16360ba90d4c9cf
ivannemeth <nemethivn@gmail.com>
2023-11-27 17:18:41 +0100

    button

--- react-props/react-props_button/src/App.js
+++ react-props/react-props_button/src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <Button
+      text="Submit"
+      colorB="green"
+      onClick={() => console.log("You clicked me!")}
+    />
+  );
+}
+
+function Button({ colorB, disabled, text, onClick }) {
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        style={{ backgroundColor: colorB }}
+      >
+        {text}
+      </button>
+    </div>
+  );
 }
