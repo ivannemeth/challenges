4e1911d370ff046237eb7e1e3846b247e727f187
ivannemeth <nemethivn@gmail.com>
2023-11-27 15:57:09 +0100

    smiley

--- react-props/react-props_smiley/src/App.js
+++ react-props/react-props_smiley/src/App.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley />;
+}
+
+function Smiley({ isHappy }) {
+  return (
+    <div>{isHappy ? <span>happy emoji</span> : <span>sad emoji</span>}</div>
+  );
 }
