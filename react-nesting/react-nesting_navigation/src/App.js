4b64de588b92b660b479e1a77ebf783c7e6bfaff
ivannemeth <nemethivn@gmail.com>
2023-11-28 12:29:41 +0100

    nesting navigation

--- react-nesting/react-nesting_navigation/src/App.js
+++ react-nesting/react-nesting_navigation/src/App.js
@@ -1,26 +1,21 @@
 import "./styles.css";
-
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Link from "./components/Link";
+import Logo from "./components/Logo";
 import avatar from "./img/avatar.jpg";
-import logo from "./img/logo.jpg";
 
 export default function App() {
   return (
     <>
-      <header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
-        <nav>
-          <a className="navigation__link" href="#home">
-            Home
-          </a>
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#impressum">
-            Impressum
-          </a>
-        </nav>
+      <Header>
+        <Navigation>
+          <Logo />
+
+          <Link href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#impressum">Impressum</Link>
+        </Navigation>
         <button
           type="button"
           onClick={() => console.log("I could toggle a profile!")}
@@ -28,7 +23,7 @@ export default function App() {
         >
           <img className="round-image" src={avatar} alt="avatar" />
         </button>
-      </header>
+      </Header>
       <main>content goes here…</main>
     </>
   );
---
+++ react-nesting/react-nesting_navigation/src/components/Logo.js
@@ -0,0 +1,10 @@
+import Image from "./components/Image";
+import logo from "./img/logo.jpg";
+
+export function Logo() {
+  return (
+    <>
+      <Image src={logo} alt="alt" />
+    </>
+  );
+}
