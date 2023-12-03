135e72610e990bf4802c032a5e5e89d9b9da70c4
ivannemeth <nemethivn@gmail.com>
2023-12-03 17:23:41 +0100

    iss

--- react-effects/react-effects-and-fetch_iss-tracker/src/App.js
+++ react-effects/react-effects-and-fetch_iss-tracker/src/App.js
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
-
-const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function App() {
   const [coords, setCoords] = useState({
@@ -11,7 +9,28 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  useEffect(() => {
+    const timer = setInterval(() => {
+      getISSCoords();
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  async function getISSCoords() {
+    const response = await fetch(
+      "https://api.wheretheiss.at/v1/satellites/25544"
+    );
+    const coords = await response.json();
+
+    console.log(coords);
+    console.log(coords.longitude);
+    console.log(coords.latitude);
+
+    setCoords({ longitude: coords.longitude, latitude: coords.latitude });
+  }
 
   return (
     <main>
