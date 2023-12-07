8086fe01854c6fe00f6b89463f805fe20045b903
ivannemeth <nemethivn@gmail.com>
2023-12-07 12:31:18 +0100

    dynamic movie section

--- nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,26 +1,31 @@
 import Link from "next/link";
 import { introduction } from "../../lib/data";
+import { useRouter } from "next/router";
+import { volumes } from "../../lib/data.js";
 
 export default function Volumes() {
+  const router = useRouter();
+
   return (
     <>
-      <h1>The Lord of the Rings</h1>
+      <h1>{volumes.title}</h1>
       <p>{introduction}</p>
       <ul>
         <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
+          <Link href={`/volumes/${volumes.slug}`}>{volumes.title}</Link>
         </li>
       </ul>
+
+      <button
+        onClick={() => {
+          function getRandomElement(array) {
+            return array[Math.floor(Math.random() * array.length)];
+          }
+          router.push(`/volumes/${getRandomElement(volumes).slug}`);
+        }}
+      >
+        Random volume
+      </button>
     </>
   );
 }
