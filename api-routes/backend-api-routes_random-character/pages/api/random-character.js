b8cc8008c873cfa3530d9a80c1ccca4258b8032d
ivannemeth <nemethivn@gmail.com>
2024-01-08 11:35:12 +0100

    random character final

--- api-routes/backend-api-routes_random-character/pages/api/random-character.js
+++ api-routes/backend-api-routes_random-character/pages/api/random-character.js
@@ -1,11 +1,13 @@
 import Chance from "chance";
 
 const chance = new Chance();
-export default function handler(request, response) {
-  response.status(200).json;
 
+export default function handler(request, response) {
   const character = {
     firstName: chance.first(),
     lastName: chance.last(),
+    twitterName: chance.twitter(),
+    geohash: chance.geohash(),
   };
+  response.status(200).json(character);
 }
--- api-routes/backend-api-routes_random-character/pages/index.js
+++ api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,19 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <div>Error</div>;
+  if (!data) return <div>loading...</div>;
+
+  return (
+    <>
+      <h1>Random characters</h1>
+      <p>First name: {data.firstName}</p>
+      <p>Last name: {data.lastName}</p>
+      <p>Twitter: {data.twitterName}</p>
+    </>
+  );
 }
